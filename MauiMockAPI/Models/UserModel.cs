5dfcb6f60a14fda1c39c8fcae54a215e9149870b
Harold <secuzi@gmail.com>
2025-04-25 10:41:04 +0800

    Changed attributes for UserModel

--- MauiMockAPI/Models/UserModel.cs
+++ MauiMockAPI/Models/UserModel.cs
@@ -11,6 +11,8 @@ namespace MauiMockAPI.Models
         public string UserId { get; set; }
         public string CreatedAt { get; set; }
         public string Name { get; set; }
+        public string CommitMessage { get; set; }
+        public string Branch { get; set; }
         public string Avatar { get; set; }
 
     }
